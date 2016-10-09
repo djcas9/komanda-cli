889d584ca876d652b8ea3b38035d09a48d3c7774
Dustin Willis Webber <dustin.webber@gmail.com>
2016-10-09 11:55:47 -0400

    version gump

--- komanda/version.go
+++ komanda/version.go
@@ -16,7 +16,7 @@ const Name = "komanda"
 const Description = "IRC Client"
 
 // Version number
-const Version = "0.4.0"
+const Version = "0.4.1"
 
 // Website number
 const Website = "github.com/mephux/komanda"
