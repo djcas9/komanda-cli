3b99c0efc1d1a10d7928319bcf85f028ac9fc152
Dustin Willis Webber <dustin.webber@gmail.com>
2017-01-23 20:42:15 -0500

    version 0.9.4

--- komanda/version/version.go
+++ komanda/version/version.go
@@ -19,7 +19,7 @@ const Name = "komanda"
 const Description = "The Komanda Command-line IRC Client"
 
 // Version number
-const Version = "0.9.3"
+const Version = "0.9.4"
 
 // Website number
 const Website = "github.com/mephux/komanda"
